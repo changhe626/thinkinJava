8d505fb0130980eb2c2da4e338d7633b9b94fb1a
changhe626 <changhe626@163.com>
2017-10-30 20:44:57 +0800

    2017年10月30日20:44:51

---
+++ src/com/javaweb/CompileDemo.java
@@ -0,0 +1,19 @@
+package com.javaweb;
+
+/**
+ * Created by zk on 2017/10/30.
+ * 作用: com.javaweb.
+ */
+public class CompileDemo {
+
+    public static void main(String[] args) {
+
+        int compile = compile(new String[]{"1","2"});
+        System.out.println(compile);
+    }
+
+    public static int compile(String[] args){
+        com.sun.tools.javac.main.Main compiler = new com.sun.tools.javac.main.Main("javac");
+        return compiler.compile(args).exitCode;
+    }
+}
