40b233d998751b3589ff663b80c6a5a847cac548
changhe626 <changhe626@163.com>
2017-12-23 16:09:38 +0800

    自己实现了classLoader 2017年12月23日16:09:33

---
+++ src/classload/ClassLoaderTest.java
@@ -0,0 +1,16 @@
+package classload;
+
+/**
+ * Created by zk on 2017/12/23.
+ * 作用: classload.
+ * 测试java类的热加载
+ *
+ *
+ * 注意IDEA不是实时编译的,请在eclipse上debug试验
+ */
+public class ClassLoaderTest {
+
+    public static void main(String[] args) {
+        new Thread(new MsgHandle()).start();
+    }
+}
---
+++ src/classload/MyManager.java
@@ -0,0 +1,15 @@
+package classload;
+
+/**
+ * Created by zk on 2017/12/23.
+ * 作用: classload.
+ * 实现接口,此类需要实现java类的热加载功能
+ */
+public class MyManager implements BaseManager {
+
+    @Override
+    public void logic() {
+        System.out.println("zhaojun i miss .... ");
+        System.out.println("today is 周六,");
+    }
+}
