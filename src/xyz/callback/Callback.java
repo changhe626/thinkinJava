431af56f4aafeda6c4cee207918962c2e4224a55
zhangke <changhe626@163.com>
2018-02-27 21:03:14 +0800

    回调函数示例 2018年2月27日21:03:09

---
+++ src/xyz/callback/Callback.java
@@ -0,0 +1,10 @@
+package xyz.callback;
+
+/**
+* 回调接口
+*/
+public interface Callback {
+
+   public void tellAnswer(int answer);
+   
+}
---
+++ src/xyz/callback/Student.java
@@ -0,0 +1,10 @@
+package xyz.callback;
+
+/**
+* 学生接口
+*/
+public interface Student {
+   
+   public void resolveQuestion(Callback callback);
+   
+}
